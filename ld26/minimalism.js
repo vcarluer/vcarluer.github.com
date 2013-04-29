a5e73af5c72af6da8104f08627046bfd659e75eb
vcarluer <vcarluer@gmail.com>
2013-04-29 02:17:19 +0200

    Minimalism

---
+++ ld26/minimalism.js
@@ -0,0 +1,38 @@
+var solutionFound = false;
+
+$(document).ready(function () {
+	$("#q").text("0");
+});
+
+$(document).click(function() {
+	if (!solutionFound) {
+		badAnswer();
+	}
+});
+
+$(document).keyup(function(key) {
+	if (!solutionFound) {
+		if (key.which == 97) {
+			$("#q").text("1");
+			$("#info").text("YOU ARE THE BEST");
+			$("#info").css("color", "green");		
+			solutionFound = true;
+			$("#info").fadeTo(0, 1);
+		} else {
+			badAnswer();
+		}
+	}
+});
+
+function badAnswer() {
+	$("#info").text("ERROR");
+			$("#info").css("color", "red");
+			$("#info").delay(300).fadeOut(100, function () { 
+				$("#info").fadeTo(0, 1);
+				if (!solutionFound) {
+					$("#info").text("");
+				} else {
+					$("#info").text("YOU ARE THE BEST");
+				}
+			});
+}
